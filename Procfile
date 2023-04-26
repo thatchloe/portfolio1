80e4ac9728ba86d57fbdf5774f9972fa8cc28a4e
thatchloe <luonghoanganh9801@gmail.com>
2023-04-27 00:13:34 +0200

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hoanganh.wsgi --log-file -
---
+++ portfolio/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hapypy\desktop\study\django-portfolio-website\portfolio\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

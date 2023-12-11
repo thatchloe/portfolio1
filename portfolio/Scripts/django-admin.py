48fb8c7b5b17b0619e0b36de4e795caa0501f89c
lương hoàng anh <anh@luongs-MacBook-Air.local>
2023-12-11 16:34:09 +0100

    .

--- portfolio/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\hapypy\desktop\study\django-portfolio-website\portfolio\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
